7b4e9d4bba9ddd7d156077260f7e253a4d869344
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2022-04-28 20:24:01 +0100

    feat: Finished Todolist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { TaskListComponent } from './task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TaskListComponent },
+  { path: 'task-detail/:id', component: TaskDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
